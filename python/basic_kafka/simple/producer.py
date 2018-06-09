788bca67f882c98f8dd59d05e7673d3f3fcd547f
Mils <mils@prontomarketing.com>
2018-06-09 10:07:59 +0700

    Make producer to send message :koala:

--- python/basic_kafka/simple/producer.py
+++ python/basic_kafka/simple/producer.py
@@ -2,7 +2,12 @@ from confluent_kafka import Producer
 
 
 def main():
-    pass
+    producer = Producer({
+        'bootstrap.servers': 'localhost:9092' #if more than one node, it will have to register here
+    })
+    producer.produce('test-topic', b'Hello, Kafka') #receive data in byete array
+    producer.flush()      #asynchronous sent
 
 if __name__ == '__main__':
     main()
+    print('Completed')
